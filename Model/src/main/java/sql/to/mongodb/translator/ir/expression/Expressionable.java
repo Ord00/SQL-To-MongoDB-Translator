a6d18ad9fb1f0d1192576c25f579dc0c246ccfea
Ord00 <ivansimonov7@yandex.ru>
2026-04-27 19:13:43 +0300

    Resolved serializing getters issues

--- Model/src/main/java/sql/to/mongodb/translator/ir/expression/Expressionable.java
+++ Model/src/main/java/sql/to/mongodb/translator/ir/expression/Expressionable.java
@@ -2,7 +2,13 @@ package sql.to.mongodb.translator.ir.expression;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import sql.to.mongodb.translator.ir.Aggregate;
+import sql.to.mongodb.translator.ir.Constant;
 import sql.to.mongodb.translator.ir.Field;
+import sql.to.mongodb.translator.ir.GroupByField;
+import sql.to.mongodb.translator.ir.SortField;
+import sql.to.mongodb.translator.ir.Subquery;
+import sql.to.mongodb.translator.ir.projection.ProjectionField;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
@@ -10,10 +16,17 @@ import sql.to.mongodb.translator.ir.Field;
         property = "@type"
 )
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = Field.class, name = "field"),
+        @JsonSubTypes.Type(value = CaseExpression.class, name = "case"),
+        @JsonSubTypes.Type(value = CaseExpression.WhenThen.class, name = "when"),
+        @JsonSubTypes.Type(value = Subquery.class, name = "subquery"),
+        @JsonSubTypes.Type(value = Aggregate.class, name = "aggregate"),
         @JsonSubTypes.Type(value = BinaryOperation.class, name = "binary"),
-        @JsonSubTypes.Type(value = UnaryOperation.class, name = "unary"),
-        @JsonSubTypes.Type(value = CaseExpression.class, name = "case")
+        @JsonSubTypes.Type(value = Constant.class, name = "constant"),
+        @JsonSubTypes.Type(value = Field.class, name = "field"),
+        @JsonSubTypes.Type(value = GroupByField.class, name = "group"),
+        @JsonSubTypes.Type(value = ProjectionField.class, name = "projection"),
+        @JsonSubTypes.Type(value = SortField.class, name = "sort"),
+        @JsonSubTypes.Type(value = UnaryOperation.class, name = "unary")
 })
 public interface Expressionable {
 }
